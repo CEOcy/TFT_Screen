7af424a78b300cde75bd8a6ae66e0efb2befa8c3
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-25 14:11:06 +0000

    Tiny changes

---
+++ app/Http/Kernel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array<int, class
