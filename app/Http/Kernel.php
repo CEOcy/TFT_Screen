cd84f084ef31d6b02be203d307cbf1f54b999fbe
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-26 16:58:04 +0000

    updated wants

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,4 +46,14 @@ class Kernel extends HttpKernel
     ];
 
     /**
-     * The application's route
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array<string, class-string|string>
+     */
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+        'cache.headers' => \Illuminate\H
