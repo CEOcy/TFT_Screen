631ecfb8ef4ba75caec91b1384ac722fa22d7ca5
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-26 22:13:18 +0000

    email changes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,4 +62,6 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\A
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    ];
+}
