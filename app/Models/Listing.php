f9fe21b163f02db855f4f4260d8dc495c9bc2452
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-28 19:48:23 +0000

    Updated licence

---
+++ app/Models/Listing.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+
+    // protected $fillable = ['title', 'company', 'location', 'website', 'email', 'description', 'tags'];
+
+    public function scopeFilter($query, array $filters) {
+        if($filters['tag'] ?? 
