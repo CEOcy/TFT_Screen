83d132515c232aa4ea73425a34b04df2ce57d83a
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-28 05:55:22 +0000

    lib/vsprintf.c: kptr_restrict: fix pK-error in SysRq show-all-timers(Q)

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,4 +12,10 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($r
+    protected function redirectTo($request)
+    {
+        if (! $request->expectsJson()) {
+            return route('login');
+        }
+    }
+}
