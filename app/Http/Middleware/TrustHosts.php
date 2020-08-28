9db6760c4dda412857d0ceda40cfae2c68d2fa78
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-28 15:39:07 +0000

    Fix typo in README.

---
+++ app/Http/Middleware/TrustHosts.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustHosts as Middleware;
+
+class TrustHosts extends Middleware
+{
+    /**
+     * Get the host patterns that should be trusted.
+     *
+     * @return array<int, string|null>
+     */
+    publ
