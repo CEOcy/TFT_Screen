aa43b21a36d14aec4a2d9b0554c845d2bfd74bb2
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-28 20:41:47 +0000

    Permissions fix

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -12,4 +12,16 @@ class Listing extends Model
     // protected $fillable = ['title', 'company', 'location', 'website', 'email', 'description', 'tags'];
 
     public function scopeFilter($query, array $filters) {
-        if($filters['tag'] ?? 
+        if($filters['tag'] ?? false) {
+            $query->where('tags', 'like', '%' . request('tag') . '%');
+        }
+
+        if($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('description', 'like', '%' . request('search') . '%')
+                ->orWhere('tags', 'like', '%' . request('search') . '%');
+        }
+    }
+
+    // Relationship To User
+    public function user() {
