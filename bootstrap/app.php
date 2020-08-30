717c91b3999c015bc6a5cbe7b99e6e2d805f8c15
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-30 19:28:57 +0000

    Bumped package version

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,4 +21,25 @@ $app = new Illuminate\Foundation\Application(
 |--------------------------------------------------------------------------
 |
 | Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them w
+| we will be able to resolve them when needed. The kernels serve the
+| incoming requests to this application from both the web and CLI.
+|
+*/
+
+$app->singleton(
+    Illuminate\Contracts\Http\Kernel::class,
+    App\Http\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Console\Kernel::class,
+    App\Console\Kernel::class
+);
+
+$app->singleton(
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    App\Exceptions\Handler::class
+);
+
+/*
+|---------------------------------
