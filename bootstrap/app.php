e09ad4d3fd292ef2114bb31b8100ff787305552f
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-30 13:19:54 +0000

    updated typings

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,4 +7,18 @@
 |
 | The first thing we will do is create a new Laravel application instance
 | which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system
+| the IoC container for the system binding all of the various parts.
+|
+*/
+
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+/*
+|--------------------------------------------------------------------------
+| Bind Important Interfaces
+|--------------------------------------------------------------------------
+|
+| Next, we need to bind some important interfaces into the container so
+| we will be able to resolve them w
