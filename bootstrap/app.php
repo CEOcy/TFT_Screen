de9c3ada5db3e8f36b1fd8be3a2867bbcf7ce798
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-30 21:06:02 +0000

    remove deprecation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -42,4 +42,14 @@ $app->singleton(
 );
 
 /*
-|---------------------------------
+|--------------------------------------------------------------------------
+| Return The Application
+|--------------------------------------------------------------------------
+|
+| This script returns the application instance. The instance is given to
+| the calling script so we can separate the building of the instances
+| from the actual running of the application and sending responses.
+|
+*/
+
+return $app;
