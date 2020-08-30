fd194c1cb7a58ab7ee19ca1519fceffe8eeb2d68
CEOcy <118271108+CEOcy@users.noreply.github.com>
2020-08-30 13:12:51 +0000

    Update bundle version

---
+++ bootstrap/app.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| The first thing we will do is create a new Laravel application instance
+| which serves as the "glue" for all the components of Laravel, and is
+| the IoC container for the system
