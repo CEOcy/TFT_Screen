5119547fe6e36a369663b2741b159bcb72681c89
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-10-08 16:33:13 +0000

    build(deps-dev): Bump @typescript-eslint/eslint-plugin

--- config/cors.php
+++ config/cors.php
@@ -11,4 +11,24 @@ return [
     | or "CORS". This determines what cross-origin operations may execute
     | in web browsers. You are free to adjust these settings as needed.
     |
-  
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
