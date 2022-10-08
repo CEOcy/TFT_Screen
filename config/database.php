637e7735071117bdc851d9fb598ed02c1093ddcb
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-10-08 22:34:58 +0000

    _automated renaming BT brand --> BTFG

--- config/database.php
+++ config/database.php
@@ -5,4 +5,17 @@ use Illuminate\Support\Str;
 return [
 
     /*
-    |--------------------------------------------------
+    |--------------------------------------------------------------------------
+    | Default Database Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the database connections below you wish
+    | to use as your default connection for all database work. Of course
+    | you may use many connections at once using the Database library.
+    |
+    */
+
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    /*
+    |-------------------------------------------------------------------------
