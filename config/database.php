bb67bbf1339e04959be63ed9966fac3d542d1438
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-10-14 11:22:43 +0000

    rename README

--- config/database.php
+++ config/database.php
@@ -28,4 +28,21 @@ return [
     |
     |
     | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have th
+    | so make sure you have the driver for your particular database of
+    | choice installed on your machine before you begin development.
+    |
+    */
+
+    'connections' => [
+
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'ur
