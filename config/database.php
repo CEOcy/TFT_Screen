c9f6da698859bf571d2c84115beb02c22253417c
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-10-16 08:24:15 +0000

    fixed linking

--- config/database.php
+++ config/database.php
@@ -80,4 +80,14 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'url
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '1433'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
+            // 'trust_s
