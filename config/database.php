6ba3e000e0eda03d774c687f616f75b88e091265
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-10-18 09:58:19 +0000

    Rename constants

--- config/database.php
+++ config/database.php
@@ -141,4 +141,11 @@ return [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
+        ],
+
+    ],
+
+];
---
+++ config/filesystems.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default filesystem disk that should be used
+    | by the framework. The "lo
