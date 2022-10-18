46a503f0dbe19e0874a55858ed7628aab9b50961
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-10-18 22:37:20 +0000

    Bump version to 0.0.15

--- config/filesystems.php
+++ config/filesystems.php
@@ -8,4 +8,17 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify the default filesystem disk that should be used
-    | by the framework. The "lo
+    | by the framework. The "local" disk, as well as a variety of cloud
+    | based disks are available to your application. Just store away!
+    |
+    */
+
+    'default' => env('FILESYSTEM_DISK', 'public'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure as many filesystem "disks" as you wish, and you
+   
