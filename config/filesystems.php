6846d8def4bd2a60431fa770c093055296c6bd59
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-10-19 07:16:56 +0000

    usb: gadget: midi: free hs descriptors

--- config/filesystems.php
+++ config/filesystems.php
@@ -21,4 +21,22 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may configure as many filesystem "disks" as you wish, and you
-   
+    | may even configure multiple disks of the same driver. Defaults have
+    | been set up for each driver as an example of the required values.
+    |
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
+    |
+    */
+
+    'disks' => [
+
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+     
