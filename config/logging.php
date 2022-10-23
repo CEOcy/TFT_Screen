e4752ef186fc28054ed697fe03d2e86383636d09
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-10-23 18:53:44 +0000

    - extended documentation

--- config/logging.php
+++ config/logging.php
@@ -7,4 +7,17 @@ use Monolog\Handler\SyslogUdpHandler;
 return [
 
     /*
-    |-------------------
+    |--------------------------------------------------------------------------
+    | Default Log Channel
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the default log channel that gets used when writing
+    | messages to the logs. The name specified in this option should match
+    | one of the channels defined in the "channels" configuration array.
+    |
+    */
+
+    'default' => env('LOG_CHANNEL', 'stack'),
+
+    /*
+    |-------
