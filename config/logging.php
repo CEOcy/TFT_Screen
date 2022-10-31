5b9d057f4e2d3101683487241b0e355fe468f08c
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-10-31 15:40:34 +0000

    ASoC: wm8994: missing break in wm8994_aif3_hw_params()

--- config/logging.php
+++ config/logging.php
@@ -61,4 +61,20 @@ return [
         ],
 
         'daily' => [
-            'd
+            'driver' => 'daily',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+        ],
+
+        'slack' => [
+            'driver' => 'slack',
+            'url' => env('LOG_SLACK_WEBHOOK_URL'),
+            'username' => 'Laravel Log',
+            'emoji' => ':boom:',
+            'level' => env('LOG_LEVEL', 'critical'),
+        ],
+
+        'papertrail' => [
+            'driver' => 'monolog',
+            'level' => env('LOG_
