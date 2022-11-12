c592001a73f7b23b756542239e99c89f864bf981
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-11-12 11:23:24 +0000

    - updated readme

--- config/sanctum.php
+++ config/sanctum.php
@@ -5,4 +5,16 @@ use Laravel\Sanctum\Sanctum;
 return [
 
     /*
-    |-------------------------------------------------------
+    |--------------------------------------------------------------------------
+    | Stateful Domains
+    |--------------------------------------------------------------------------
+    |
+    | Requests from the following domains / hosts will receive stateful API
+    | authentication cookies. Typically, these should include your local
+    | and production domains which access your API via a frontend SPA.
+    |
+    */
+
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+        '%s%s',
+        'localhost,lo
