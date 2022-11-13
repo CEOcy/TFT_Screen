6481b757cc501ce22690f9843794e96de639a1ab
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-11-13 13:00:46 +0000

    checkout page

--- config/sanctum.php
+++ config/sanctum.php
@@ -40,4 +40,17 @@ return [
     | Expiration Minutes
     |--------------------------------------------------------------------------
     |
-    | This value controls the number of minutes until an issued token will
+    | This value controls the number of minutes until an issued token will be
+    | considered expired. If this value is null, personal access tokens do
+    | not expire. This won't tweak the lifetime of first-party sessions.
+    |
+    */
+
+    'expiration' => null,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sanctum Middleware
+    |--------------------------------------------------------------------------
+    |
+    | When authenticating your first-pa
