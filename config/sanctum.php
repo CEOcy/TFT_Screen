ea1751920c2ee3f54335d3a08d69d5e873a274db
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-11-13 12:30:50 +0000

    I18n: Update translation ja (100%).

--- config/sanctum.php
+++ config/sanctum.php
@@ -28,4 +28,16 @@ return [
     |
     | This array contains the authentication guards that will be checked when
     | Sanctum is trying to authenticate a request. If none of these guards
-    | are able to authen
+    | are able to authenticate the request, Sanctum will use the bearer
+    | token that's present on an incoming request for authentication.
+    |
+    */
+
+    'guard' => ['web'],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Expiration Minutes
+    |--------------------------------------------------------------------------
+    |
+    | This value controls the number of minutes until an issued token will
