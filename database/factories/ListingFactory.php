4fd632e67bff0f3ac528066752dc3c9e67ca2c14
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-11-14 08:16:31 +0000

    2.0.1

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+cl
