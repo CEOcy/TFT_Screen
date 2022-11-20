39e7167159bffc3c6b8dc3ea03a84f71c36a1151
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-11-20 23:04:51 +0000

    Update schema

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -33,4 +33,10 @@ class UserFactory extends Factory
      */
     public function unverified()
     {
-        return $this->state(function (array $attribut
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}
