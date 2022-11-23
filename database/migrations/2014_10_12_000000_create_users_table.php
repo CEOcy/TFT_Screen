8e2b8ef27a19f41c9aa2abad1058eabd90a6dc20
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-11-23 13:35:24 +0000

    Create new file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,4 +13,23 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', functio
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
