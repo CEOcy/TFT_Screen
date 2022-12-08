0090007e684508040237bf02ed037601278cdcf9
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-12-08 07:31:47 +0000

    copyedits

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,4 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('tokenable');
+            $table->string('name');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    publi
