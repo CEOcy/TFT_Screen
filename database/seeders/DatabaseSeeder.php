67c364b9986a33f0da582a326c38816171c5b3e9
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-12-13 13:02:06 +0000

    fix ci

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,4 +13,22 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      *
-     * @return voi
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(5)->create();
+
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
+
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer', 
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
