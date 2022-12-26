d5f99235b8bfb738db907f7ac59ab3f45009fefd
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-12-26 22:35:40 +0000

    add request

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,3 +32,11 @@ class DatabaseSeeder extends Seeder
         //     'title' => 'Laravel Senior Developer', 
         //     'tags' => 'laravel, javascript',
         //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Full-Stack Engineer'
