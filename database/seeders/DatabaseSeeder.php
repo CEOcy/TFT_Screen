7b47331d2979f1e263348fe41e73da6b0ad3df15
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-12-27 08:42:55 +0000

    0.1.31

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -45,4 +45,7 @@ class DatabaseSeeder extends Seeder
         //     'location' => 'New York, NY',
         //     'email' => 'email2@email.com',
         //     'website' => 'https://www.starkindustries.com',
-        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveni
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        //   ]);
+    }
+}
---
+++ lang/en/auth.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | 
