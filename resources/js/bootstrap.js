910ed98174a5bd17d9f9c849931a07b51b0cb122
CEOcy <118271108+CEOcy@users.noreply.github.com>
2022-12-28 11:13:00 +0000

    Fix typo in method name

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -23,4 +23,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     forceTLS: true
+// });
