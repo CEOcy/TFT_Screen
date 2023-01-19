e1b72e370e132c2f598b5dbab7d7f06df60a0f88
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-01-19 10:20:30 +0000

    version 1.1.3

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -1,2 +1,12 @@
 <x-layout>
-  <x-card class="p-10 max-w-lg mx-auto m
+  <x-card class="p-10 max-w-lg mx-auto mt-24">
+    <header class="text-center">
+      <h2 class="text-2xl font-bold uppercase mb-1">Create a Gig</h2>
+      <p class="mb-4">Post a gig to find a developer</p>
+    </header>
+
+    <form method="POST" action="/listings" enctype="multipart/form-data">
+      @csrf
+      <div class="mb-6">
+        <label for="company" class="inline-block text-lg mb-2">Company Name</label>
+        <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company
