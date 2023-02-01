db01b7c713614c476e3ef79332e57f2185bdd1be
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-01 19:50:52 +0000

    s390/kvm: Fix store status for ACRS/FPRS

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -36,3 +36,18 @@
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
         @enderror
       </div>
+
+      <div class="mb-6">
+        <label for="email" class="inline-block text-lg mb-2">
+          Contact Email
+        </label>
+        <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}" />
+
+        @error('email')
+        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+        @enderror
+      </div>
+
+      <div class="mb-6">
+        <label for="website" class="inline-block text-lg mb-2">
+          Website/Application
