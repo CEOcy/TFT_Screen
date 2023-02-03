60ca3b9ad79a3346e1c66250a09f82e860292b66
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-03 18:31:13 +0000

    removed useless comments

--- resources/views/listings/edit.blade.php
+++ resources/views/listings/edit.blade.php
@@ -1,16 +1,18 @@
+
 <x-layout>
   <x-card class="p-10 max-w-lg mx-auto mt-24">
     <header class="text-center">
-      <h2 class="text-2xl font-bold uppercase mb-1">Create a Gig</h2>
-      <p class="mb-4">Post a gig to find a developer</p>
+      <h2 class="text-2xl font-bold uppercase mb-1">Edit Gig</h2>
+      <p class="mb-4">Edit: {{$listing->title}}</p>
     </header>
 
-    <form method="POST" action="/listings" enctype="multipart/form-data">
+    <form method="POST" action="/listings/{{$listing->id}}" enctype="multipart/form-data">
       @csrf
+      @method('PUT')
       <div class="mb-6">
         <label for="company" class="inline-block text-lg mb-2">Company Name</label>
         <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
-          value="{{old('company')}}" />
+          value="{{$listing->company}}" />
 
         @error('company')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -20,7 +22,7 @@
       <div class="mb-6">
         <label for="title" class="inline-block text-lg mb-2">Job Title</label>
         <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
-          placeholder="Example: Senior Laravel Developer" value="{{old('title')}}" />
+          placeholder="Example: Senior Laravel Developer" value="{{$listing->title}}" />
 
         @error('title')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -30,7 +32,7 @@
       <div class="mb-6">
         <label for="location" class="inline-block text-lg mb-2">Job Location</label>
         <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
-          placeholder="Example: Remote, Boston MA, etc" value="{{old('location')}}" />
+          placeholder="Example: Remote, Boston MA, etc" value="{{$listing->location}}" />
 
         @error('location')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -41,7 +43,7 @@
         <label for="email" class="inline-block text-lg mb-2">
           Contact Email
         </label>
-        <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{old('email')}}" />
+        <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" value="{{$listing->email}}" />
 
         @error('email')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -53,7 +55,7 @@
           Website/Application URL
         </label>
         <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website"
-          value="{{old('website')}}" />
+          value="{{$listing->website}}" />
 
         @error('website')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
@@ -65,43 +67,7 @@
           Tags (Comma Separated)
         </label>
         <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
-          placeholder="Example: Laravel, Backend, Postgres, etc" value="{{old('tags')}}" />
+          placeholder="Example: Laravel, Backend, Postgres, etc" value="{{$listing->tags}}" />
 
         @error('tags')
         <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-        @enderror
-      </div>
-
-      <div class="mb-6">
-        <label for="logo" class="inline-block text-lg mb-2">
-          Company Logo
-        </label>
-        <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo" />
-
-        @error('logo')
-        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-        @enderror
-      </div>
-
-      <div class="mb-6">
-        <label for="description" class="inline-block text-lg mb-2">
-          Job Description
-        </label>
-        <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
-          placeholder="Include tasks, requirements, salary, etc">{{old('description')}}</textarea>
-
-        @error('description')
-        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
-        @enderror
-      </div>
-
-      <div class="mb-6">
-        <button class="bg-laravel text-white rounded py-2 px-4 hover:bg-black">
-          Create Gig
-        </button>
-
-        <a href="/" class="text-black ml-4"> Back </a>
-      </div>
-    </form>
-  </x-card>
-</x-layout>
