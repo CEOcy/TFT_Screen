7a136cb2439cfe8a32764d262c4e7928c7bb65c4
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-05 13:59:24 +0000

    Update quickstart

--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -3,4 +3,18 @@
     style="background-image: url('images/laravel-logo.png')"></div>
 
   <div class="z-10">
-    <h1 clas
+    <h1 class="text-6xl font-bold uppercase text-white">
+      Lara<span class="text-black">Gigs</span>
+    </h1>
+    <p class="text-2xl text-gray-200 font-bold my-4">
+      Find or post Laravel jobs & projects
+    </p>
+    <div>
+      @auth
+      @else
+      <a href="/register"
+        class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign
+        Up to List a Gig</a>
+      @endauth
+    </div>
+  </div>
