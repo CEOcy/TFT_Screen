ef9ea2f202fdbf6a6d3e2c15fa4229e1ec85166e
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-05 21:20:22 +0000

    Raised submodule pointer

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -23,4 +23,17 @@
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="active" aria-current="page"><span>{{ $page }
+                            <li class="active" aria-current="page"><span>{{ $page }}</span></li>
+                        @else
+                            <li><a href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            @endforeach
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li>
+                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                </li>
+           
