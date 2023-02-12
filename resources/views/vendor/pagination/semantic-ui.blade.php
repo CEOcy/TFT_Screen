9bf30ff96943aa88aa56c3a66082d0749a17bddf
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-12 12:15:11 +0000

    Bump Microsoft.NET.Test.Sdk from 16.8.0 to 16.8.3

--- resources/views/vendor/pagination/semantic-ui.blade.php
+++ resources/views/vendor/pagination/semantic-ui.blade.php
@@ -2,4 +2,13 @@
     <div class="ui pagination menu" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <a class="icon item disabled" a
+            <a class="icon item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
+        @else
+            <a class="icon item" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')"> <i class="left chevron icon"></i> </a>
+        @endif
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <a
