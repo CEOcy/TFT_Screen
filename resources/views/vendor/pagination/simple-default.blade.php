2fa1a7317f8eef917f686a6bf6bca55f49470231
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-14 17:03:37 +0000

    oke

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -1,3 +1,13 @@
 @if ($paginator->hasPages())
     <nav>
-        <ul class="pagin
+        <ul class="pagination">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
+            @else
+                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
+            @endif
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+       
