6aaf4628478a8e192035b728a811638def7be1d7
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-14 22:16:06 +0000

    Updated the importlib_metadata feedstock.

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -10,4 +10,10 @@
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-       
+                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+            @else
+                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
+            @endif
+        </ul>
+    </nav>
+@endif
---
+++ resources/views/vendor/pagination/simple-tailwind.blade.php
@@ -0,0 +1,4 @@
+@if ($paginator->hasPages())
+    <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between">
+        {{-- Previous Page Link --}}
+        @if ($
