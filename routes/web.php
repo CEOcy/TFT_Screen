5a25e859b7cc170a57c137b12934cd5911d2fbbc
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-16 17:39:44 +0000

    builder

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded 
