8cd5c10d1d9cfab21d52847e1b7359212ecbd33b
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-16 18:53:32 +0000

    Update commands.txt

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,21 @@ use App\Models\Listing;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded 
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing  
+
+// All Listings
+Route::get('/', [ListingController::class, 'index']);
+
+// Show Crea
