77ac0b7da38871a3f57c3762870dc911d9dc560e
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-16 08:13:24 +0000

    Add requirements section

--- resources/views/vendor/pagination/simple-tailwind.blade.php
+++ resources/views/vendor/pagination/simple-tailwind.blade.php
@@ -17,4 +17,9 @@
                 {!! __('pagination.next') !!}
             </a>
         @else
-            <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border b
+            <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md">
+                {!! __('pagination.next') !!}
+            </span>
+        @endif
+    </nav>
+@endif
