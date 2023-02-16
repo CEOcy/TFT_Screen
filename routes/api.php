8689bdbf5126191b1d46ae5c41069346c6e66ef2
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-16 08:22:56 +0000

    Gemfile.lock update

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
