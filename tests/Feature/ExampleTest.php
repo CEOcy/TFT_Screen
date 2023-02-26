d47d3da19e87c19c1fc504f6c406086e66527ed3
CEOcy <118271108+CEOcy@users.noreply.github.com>
2023-02-26 10:25:28 +0000

    Run update-remote.sh on memcached

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+    
